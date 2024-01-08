6205e8ee24eb7794256871e01bba93aa14fff448
venky-944 <113006417+venky-944@users.noreply.github.com>
2024-01-08 08:48:45 +0530

    Update Sample_cls2.java

--- src/sample_package2/Sample_cls2.java
+++ src/sample_package2/Sample_cls2.java
@@ -4,7 +4,7 @@ public class Sample_cls2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello venky");
+		System.out.println("hello pavan");
 
 	}
 
